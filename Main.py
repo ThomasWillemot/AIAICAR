69a8b2e51b90e5077b015f27ea4c25c87b8e1aca
thomaswillemot <thomas.willemot@vintecc.com>
2021-11-20 15:46:38 +0100

    Add driving commands for first test

--- Main.py
+++ Main.py
@@ -1,9 +1,7 @@
 import logging
 import time
-
 from pynput import keyboard
 import Utils.CarControls as CarControls
-
 
 
 def on_press(key):
@@ -14,12 +12,14 @@ def on_press(key):
         print('special key {0} pressed'.format(
             key))
 
+
 def on_release(key):
-    print('{0} released'.format(
-        key))
+    print('{0} released'.format(key))
     if key == keyboard.Key.esc:
         # Stop listener
         return False
+
+
 def keyboard_driving():
     # Collect events until released
     with keyboard.Listener(
@@ -34,12 +34,19 @@ def keyboard_driving():
     listener.start()
 
 
-
 def main():
     # Test each motor movement
-    car_motor_pins_config = [0,0,0,0,0,0,0,0]
-    car_controls = CarControls.CarControls(car_motor_pins_config)
+    AIn1 = 4
+    AIn2 = 2
+    AIn3 = 17
+    AIn4 = 3
 
+    BIn1 = 6
+    BIn2 = 13
+    BIn3 = 19
+    BIn4 = 26
+    car_motor_pins_config = [AIn1,AIn2,AIn3,AIn4,BIn1,BIn2,BIn3,BIn4]
+    car_controls = CarControls.CarControls(car_motor_pins_config)
     car_controls.drive_forward()
     print("Forward")
     time.sleep(5)
--- Utils/CarControls.py
+++ Utils/CarControls.py
@@ -1,28 +1,27 @@
 """
 Driving functions, should contain forward, backward, turning and driving turns in all directions.
 """
-
+import RPi.GPIO as GPIO
 
 class CarControls:
     def __init__(self,config_ports):
         # Stands for [position left/right][position front/rear][forward/backward]
-            LFF = config_ports[0]
-            LFB = config_ports[1]
-            LRF = config_ports[2]
-            LRB = config_ports[3]
-            RFF = config_ports[4]
-            RFB = config_ports[5]
-            RRF = config_ports[6]
-            RRB = config_ports[7]
+        self.config_ports = config_ports
 
     def stop(self):
+        for port in self.config_ports:
+            GPIO.output(port, GPIO.LOW)
         return True
 
     def drive_forward(self):
-        return False
+        for i in range(len(self.config_ports)):
+            if i % 2 == 0:
+                GPIO.output(self.config_ports[i], GPIO.HIGH)
 
     def drive_backward(self):
-        return False
+        for i in range(len(self.config_ports)):
+            if i % 2 == 1:
+                GPIO.output(self.config_ports[i], GPIO.HIGH)
 
     def rotate_left(self):
         return False
@@ -41,3 +40,4 @@ class CarControls:
 
     def drive_back_right(self):
         return False
+
