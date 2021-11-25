30caa06730560dd2c52cd78e11cf35e56a236b28
thomaswillemot <thomas.willemot@vintecc.com>
2021-11-25 11:10:59 +0100

    Add steering test

--- Utils/SteeringTest.py
+++ Utils/SteeringTest.py
@@ -5,16 +5,20 @@ curses.cbreak()
 screen.keypad(True)
 
 def main():
-
-    char = screen.getch()
-
-    if char == 'w':
-        print('forward')
-    if char == 's':
-        print('backward')
-    if char == ' ':
-        print('jump')
-
+    try:
+        while(1):
+            char = screen.getch()
+            if char == 'w':
+                print('forward')
+            if char == 's':
+                print('backward')
+            if char == ' ':
+                print('jump')
+    except:
+        curses.nocbreak()
+        screen.keypad(False)
+        curses.echo()
+        curses.endwin()
 
 if __name__ =="__main__":
     main()
